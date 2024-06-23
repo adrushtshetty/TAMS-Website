cf0d18b6cd0e96bed5917c4a0a8c8ad8ebf01085
adrushtshetty <adrushtshetty@gmail.com>
2024-06-23 09:43:15 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify, render_template, url_for, redirect
+#from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+@app.route('/')
+def home_page():
+    return render_template("index.html")
+
+@app.route('/about')
+def about_page():
+    return render_template("venue.html")
+
+@app.route('/aboutPESU')
+def about_PESU_page():
+    return render_template("about.html")
+
+@app.route('/events')
+def about_events_page():
+    return render_template("Program.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
